b4b6133876f7096a2a470109f7616871111356a9
Edgar Esparza <edgar.i.esparza@gmail.com>
2021-02-13 14:52:46 -0600

    added movidb stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+let baseUrl = "https://api.themoviedb.org/3/movie/now_playing";
+let apikey = "?api_key=e725fe3ed32db32d41d976be391ef0df";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    currentMovies: []
+  },
+  mutations: {
+    SET_CURRENT_MOVIES: (state, data) => (state.currentMovies = data)
+  },
+  actions: {
+    getCurrentMovies({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`${baseUrl + apikey}`)
+          .then(response => {
+            commit("SET_CURRENT_MOVIES", response.data.results);
+            resolve();
+          })
+          .catch(err => {
+            console.log(err);
+            reject();
+          });
+      });
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div
+      class="movies_container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+    >
+      <div v-for="(movie, i) in currentMovies" :key="i" class="movie">
+        <img
+          class="movie__poster"
+          :src="`https://image.tmdb.org/t/p/w500` + movie.poster_path"
+          alt=""
+        />
+        <div class="movie__info">
+          <h2 class="movie__info--title">{{ movie.title }}</h2>
+          <p class="movie__info--overview">{{ movie.overview }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mapState } from "vuex";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState(["currentMovies"])
   }
 };
 </script>
+<style lang="scss" scoped>
+.movie {
+  &:hover {
+    .movie__info {
+      background-color: rgba(0, 0, 0, 0.7);
+      z-index: 10;
+    }
+  }
+  &__info {
+    position: absolute;
+    height: 100%;
+    z-index: -1;
+  }
+}
+</style>
